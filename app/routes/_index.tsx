b748379734b050fd5f9c4bc87dd6a9fa748a6faa
kimizuy <kimizuyamasaki@gmail.com>
2025-04-20 16:40:31 +0900

    add layout components

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,8 @@
 import type { Route } from "./+types/_index";
 import { TodoApp } from "./components/todo-app";
+import { Header } from "~/components/layout/header";
+import { Main } from "~/components/layout/main";
+import { TopNav } from "~/components/layout/top-nav";
 import { ThemeSwitch } from "~/components/theme-switch";
 
 export function meta(_: Route.MetaArgs) {
@@ -15,9 +18,31 @@ export function loader({ context }: Route.LoaderArgs) {
 
 export default function Home({ loaderData }: Route.ComponentProps) {
   return (
-    <div>
-      <ThemeSwitch />
-      <TodoApp />
-    </div>
+    <>
+      <Header>
+        <TopNav
+          links={[
+            {
+              title: "Home",
+              href: "/",
+            },
+            {
+              title: "About",
+              href: "/about",
+            },
+            {
+              title: "Contact",
+              href: "/contact",
+            },
+          ]}
+        />
+        <div className="ml-auto flex items-center space-x-4">
+          <ThemeSwitch />
+        </div>
+      </Header>
+      <Main>
+        <TodoApp />
+      </Main>
+    </>
   );
 }
