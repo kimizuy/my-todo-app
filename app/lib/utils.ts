24ed943b52225ac5fc7bf798424dc3bfa6c421da
kimizuy <kimizuyamasaki@gmail.com>
2025-10-10 02:02:02 +0900

    chore: remove unused function

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -15,22 +15,3 @@ export function formatDate(dateString: string) {
     minute: "2-digit",
   });
 }
-
-// biome-ignore lint/suspicious/noExplicitAny: Generic debounce function requires any for flexibility with different function signatures
-export function debounce<T extends (...args: any[]) => any>(
-  func: T,
-  delay: number,
-): (...args: Parameters<T>) => void {
-  let timeoutId: ReturnType<typeof setTimeout> | null = null;
-
-  return function debounced(...args: Parameters<T>) {
-    if (timeoutId !== null) {
-      clearTimeout(timeoutId);
-    }
-
-    timeoutId = setTimeout(() => {
-      func(...args);
-      timeoutId = null;
-    }, delay);
-  };
-}
